a50a4a8d464f8c0a39269d2e0567b4b483c26df8
mirtehouwing <mirtehouwing@gmail.com>
2022-10-27 12:40:28 +0200

    setup, packages

---
+++ src/main/java/com/example/countingwordsrest/CountingWordsRestApplication.java
@@ -0,0 +1,14 @@
+package com.example.countingwordsrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountingWordsRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountingWordsRestApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/example/countingwordsrest/CountingWordsRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.countingwordsrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountingWordsRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
